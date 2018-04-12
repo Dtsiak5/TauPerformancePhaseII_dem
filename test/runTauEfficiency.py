6903639e90ad3b439457e11ad4092872191a1e7d
Dtsiak5 <demetra.tsiakkouri@cern.ch>
2018-04-12 22:29:54 +0300

    Add files via upload

--- test/runTauEfficiency.py
+++ test/runTauEfficiency.py
@@ -11,15 +11,12 @@ options = VarParsing ('analysis')
 #options.inputFiles="/store/user/adewit/Jun22_MC_91X/GluGluHToTauTau_M125_14TeV_powheg_pythia8/crab_GluGluHToTauTau_M-125-barrel-200PU-miniAOD/170711_074522/0000/out_miniAOD_prod_99.root"
 #options.inputFiles="/store/user/agilbert/DYJetsToTauTau_M-50_TuneCUETP8M1_14TeV-madgraphMLM-pythia8/miniaod-prod-020117-0PU/170803_115641/0000/miniaod_1.root"
 #options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/public/miniAOD_TauReco_ak4PFJets_ggH_PU200.root"
-#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_ggH_PU0.root"
-#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_QCD_PU200.root"
-#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_QCD_PU0.root"
-#options.inputFiles="file:/afs/cern.ch/work/e/eerodoto/public/miniAOD_TauReco_ak4PFJets_pu200_signal.root"
-#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/phase2-tauPerformance/CMSSW_9_4_2/src/RecoTauTag/TauPerformancePhaseII/test/Plotting/Eleni_store/miniAOD_TauReco_ak4PFJets_QCD_pu.root"
-#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_GGTT_PU200.root"
-options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_ggH_PU0.root"
-options.outputFile ="new_store/TwoTaus_ggH_PU0_PUPPI.root"
-#options.outputFile ="new_store/TwoTaus_QCD_PU0_PUPPI.root"
+#options.inputFiles="file:/afs/cern.ch/user/d/dtsiakko/public/For_Usama/miniAOD_TauReco_ak4PFJets_ggH_PU200.root"
+#options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/public/miniAOD_TauReco_ak4PFJets_QCD_PU200.root"
+#options.outputFile ="TwoTaus_Demetra.root"
+options.inputFiles="file:/afs/cern.ch/work/d/dtsiakko/private/CMSSW_9_4_2_tauAtMiniAOD/src/RecoTauTag/Configuration/test/miniAOD_TauReco_ak4PFJets_QCD_PU200.root"
+#options.outputFile ="store/TwoTaus_QCD_PU200_PUPPI_last3.root"
+options.outputFile ="store/TwoTaus_QCD_PU200_PUPPI_new_cand_8800.root"
 
 options.register('inputFileList', '', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'Manual file list input, will query DAS if empty')
 options.parseArguments()
@@ -32,12 +29,11 @@ process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, '91X_upgrade2023_realistic_v3', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, '93X_upgrade2023_realistic_v2', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '91X_upgrade2023_realistic_v3', '')
 
 #how many events to run over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(9000)
 )
 
 
@@ -51,16 +47,12 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(options.inputFiles),
 )
 
-
 ##################################################
 # Main
 process.cutBased = cms.EDAnalyzer("phase2Taus",
     vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
-#    taus = cms.InputTag("selectedPatTaus"),
     taus = cms.InputTag("slimmedTaus"),
-#    tausOrg = cms.InputTag("slimmedTaus"),
     jets = cms.InputTag("slimmedJetsPuppi"),
-#   jets = cms.InputTag("slimmedJets"),
     genJets=cms.InputTag("slimmedGenJets"),
     tauID = cms.string("byCombinedIsolationDeltaBetaCorrRaw3Hits"),
     pruned = cms.InputTag("prunedGenParticles")
